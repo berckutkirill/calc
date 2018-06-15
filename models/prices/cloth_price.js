bce3addf08e2d18fb85b8f03ba0ea02fce317e92
kirill <berckutkirill@ya.ru>
2018-06-15 18:54:50 +0300

    Добавил файл

--- models/prices/cloth_price.js
+++ models/prices/cloth_price.js
@@ -1,38 +1,49 @@
 const mongoose = require('mongoose');
 const Config = mongoose.model('Config');
 const Schema = mongoose.Schema;
-
+const prices = {
+    reinforced:50
+};
 const fields = {
     cloth: {type: Schema.Types.ObjectId, ref: 'Cloth'},
     color: [{type: Schema.Types.ObjectId, ref:'Color'}],
     furnish: [{type: Schema.Types.ObjectId, ref:'Furnish'}],
     type: [{type: Schema.Types.ObjectId, ref: 'ClothType', title: "Тип"}],
     dop: [{type: Schema.Types.ObjectId, ref: 'Dop', title: "Дополнительно"}],
-    height:Number,
-    width:Number,
     patina: Boolean,
     reinforced: Boolean,
     price: Number
 };
 const clothPriceSchema = new Schema(fields);
-clothPriceSchema.methods.getPrice = function () {
+clothPriceSchema.methods.calculatePrice = function (params, config) {
+    const self = this;
+    let price = self.price;
+    let h = params['width'];
+    let w = params['height'];
+    const reinforced = params['reinforced'];
+    w = w ? w : config.standardWidths[0];
+    h = h ? h : config.standardHeights[0];
+    if (config.standardWidths.indexOf(w) === -1 || config.standardHeights.indexOf(h) === -1) {
+        let tax;
+        if (h > config.maxStandardHeight || w > config.maxStandardWidth) {
+            tax = config.cloth.sizeTax;
+        } else {
+            tax = config.cloth.bigSizeTax;
+        }
+        price *= tax;
+    }
+    if (reinforced && !self.reinforced) {
+        price += prices.reinforced;
+    }
+    const pw = w*0.1;
+    const ph = h*0.1;
+    return price*ph*pw;
+};
+clothPriceSchema.methods.getPrice = function (params) {
     const self = this;
     return new Promise(function (resolve) {
         Config.getConfig().then(function (config) {
-            const h = self['cloth']['params']['height'];
-            const w = self['cloth']['params']['width'];
-            if (config.standardWidths.indexOf(w) === -1 || config.standardHeights.indexOf(h) === -1) {
-                let tax;
-                if (h > config.maxStandardHeight || w > config.maxStandardWidth) {
-                    tax = config.cloth.sizeTax;
-                } else {
-                    tax = config.cloth.bigSizeTax;
-                }
-                const price = self.price * tax;
-                resolve(price);
-            } else {
-                resolve(self.basePrice);
-            }
+            return resolve(self.calculatePrice(params, config));
         });
     });
 };
